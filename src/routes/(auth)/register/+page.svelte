aa721b669d57dcd16c39da780ab4af26b836925a
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-06-30 23:45:17 +0100

    initial

---
+++ src/routes/(auth)/register/+page.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import GalleryVerticalEndIcon from '@lucide/svelte/icons/gallery-vertical-end';
+	import { base } from '$app/paths';
+
+	const id = $props.id();
+</script>
+
+<div class="grid min-h-svh lg:grid-cols-2">
+	<div class="flex flex-col gap-4 p-6 md:p-10">
+		<div class="flex justify-center gap-2 md:justify-start">
+			<a href="##" class="flex items-center gap-2 font-medium">
+				<div
+					class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
+				>
+					<GalleryVerticalEndIcon class="size-4" />
+				</div>
+				Bloggers
+			</a>
+		</div>
+		<div class="flex flex-1 items-center justify-center">
+			<div class="w-full max-w-xs">
+				<form class="flex flex-col gap-6">
+					<div class="flex flex-col items-center gap-2 text-center">
+						<h1 class="text-2xl font-bold">Create new account</h1>
+						<p class="text-muted-foreground text-sm text-balance">
+							Enter your email below to create your account
+						</p>
+					</div>
+					<div class="grid gap-6">
+						<div class="grid gap-3">
+							<Label for="email-{id}">Email</Label>
+							<Input id="email-{id}" type="email" placeholder="m@example.com" required />
+						</div>
+
+						<div class="grid gap-3">
+							<Label for="username-{id}">Username</Label>
+							<Input id="username-{id}" type="text" placeholder="username" required />
+						</div>
+						<div class="grid gap-3">
+							<div class="flex items-center">
+								<Label for="password-{id}">Password</Label>
+								<a href="##" class="ml-auto text-sm underline-offset-4 hover:underline">
+									Forgot your password?
+								</a>
+							</div>
+							<Input id="password-{id}" placeholder="password" type="password" required />
+						</div>
+						<Button type="submit" class="w-full">Register</Button>
+						<div
+							class="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t"
+						>
+							<span class="bg-background text-muted-foreground relative z-10 px-2">
+								Or continue with
+							</span>
+						</div>
+						<Button variant="outline" class="w-full">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+								<path
+									d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+									fill="currentColor"
+								/>
+							</svg>
+							Login with GitHub
+						</Button>
+					</div>
+					<div class="text-center text-sm">
+						Already have an account?
+						<a href="{base}/login" class="underline underline-offset-4"> Sign in </a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="bg-muted relative hidden lg:block">
+		<img
+			src="https://images.unsplash.com/photo-1432821596592-e2c18b78144f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			alt="placeholder"
+			class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+		/>
+	</div>
+</div>
