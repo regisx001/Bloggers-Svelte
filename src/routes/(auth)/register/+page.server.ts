51644a6aec5cbc35ad1cf508307fd7bddf55384c
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-07-02 00:51:49 +0100

    feat: add validation messages and enhance form structure in login and registration pages

--- src/routes/(auth)/register/+page.server.ts
+++ src/routes/(auth)/register/+page.server.ts
@@ -1,8 +1,83 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
+import { LOGIN_URL, REGISTER_URL } from '$lib/urls';
 
 export const load: PageServerLoad = ({ locals }) => {
 	if (locals.user?.accessToken) {
 		throw redirect(308, '/');
+	}
+};
+
+export const actions: Actions = {
+	default: async ({ request, cookies, fetch }) => {
+		const formData = Object.fromEntries(await request.formData());
+
+		if (!formData['username']) {
+			return {
+				username: {
+					data: formData['username'],
+					detail: "you didn't provide a username"
+				}
+			};
+		}
+
+		if (formData['password'].toString().length < 6) {
+			return {
+				password: 'password is less than 6 character',
+				username: {
+					data: formData['username']
+				}
+			};
+		}
+
+		const responseRegistration = await fetch(REGISTER_URL, {
+			method: 'POST',
+			body: JSON.stringify(formData),
+			headers: { 'content-type': 'application/json' }
+		});
+
+		if (!responseRegistration.ok) {
+			const registrationJson = await responseRegistration.json();
+
+			return {
+				username: { data: formData['username'], detail: registrationJson?.username },
+				email: { data: formData['email'], detail: registrationJson?.email },
+				password: registrationJson?.password
+			};
+		}
+
+		const loginInfo = JSON.stringify({
+			username: formData['username'],
+			password: formData['password']
+		});
+
+		const responseLogin = await fetch(LOGIN_URL, {
+			method: 'POST',
+			body: loginInfo,
+			headers: { 'content-type': 'application/json' }
+		});
+
+		const loginJson: LoginResponse = await responseLogin.json();
+
+		if (!responseLogin.ok) {
+			return {
+				error: loginJson
+			};
+		}
+
+		if (responseLogin.status === 200) {
+			cookies.set('refresh', loginJson.refreshToken, {
+				path: '/',
+				httpOnly: true,
+				maxAge: 90 * 60 * 24
+			});
+
+			cookies.set('access', loginJson.accessToken, {
+				path: '/',
+				httpOnly: true,
+				maxAge: 60 * 60
+			});
+			throw redirect(300, '/');
+		}
 	}
 };
