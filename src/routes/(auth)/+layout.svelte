0b84fb7f2400bd51d43ca33433fd10cd3dce405d
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-07-01 01:40:26 +0100

    feat: refactor authentication forms to improve structure and enhance user experience

--- src/routes/(auth)/+layout.svelte
+++ src/routes/(auth)/+layout.svelte
@@ -1,5 +1,38 @@
 <script lang="ts">
+	import GalleryVerticalEndIcon from '@lucide/svelte/icons/gallery-vertical-end';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="grid min-h-svh lg:grid-cols-2">
+	<div class="flex flex-col gap-4 p-6 md:p-10">
+		<div class="flex justify-center gap-2 md:justify-start">
+			<a href="##" class="flex items-center gap-2 font-medium">
+				<div
+					class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md"
+				>
+					<GalleryVerticalEndIcon class="size-4" />
+				</div>
+				Bloggers
+			</a>
+		</div>
+		<div class="flex flex-1 items-center justify-center">
+			<div class="w-full max-w-xs">
+				{@render children()}
+			</div>
+		</div>
+
+		<!-- <pre class="pre">
+			<code>
+				{JSON.stringify(form, null, 2)}
+			</code>
+		</pre> -->
+	</div>
+	<div class="bg-muted relative hidden lg:block">
+		<img
+			src="https://images.unsplash.com/photo-1432821596592-e2c18b78144f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			alt="placeholder"
+			class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+		/>
+	</div>
+</div>
