f5057546d96e9ac19222ad5146ee762e51a50531
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-07-01 01:32:37 +0100

    feat: update user interface to include access token and refine URL constants for account verification

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,7 +4,7 @@ declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			user: User;
+			user: User & { accessToken: string };
 		}
 		// interface PageData {}
 		// interface PageState {}
--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -2,6 +2,7 @@ import { PRIVATE_BACKEND_URL } from '$env/static/private';
 
 export const LOGIN_URL = PRIVATE_BACKEND_URL + '/auth/login';
 export const REGISTER_URL = PRIVATE_BACKEND_URL + '/auth/register';
-export const VERIFY_URL = PRIVATE_BACKEND_URL + '/auth/verify';
+export const VERIFY_ACCOUNT_URL = PRIVATE_BACKEND_URL + '/auth/verify-account';
+export const VERIFY_TOKEN_URL = PRIVATE_BACKEND_URL + '/auth/verify-token';
 export const USER_INFO_URL = PRIVATE_BACKEND_URL + '/auth/me';
 export const REFRESH_TOKEN_URL = PRIVATE_BACKEND_URL + '/auth/refresh-token';
