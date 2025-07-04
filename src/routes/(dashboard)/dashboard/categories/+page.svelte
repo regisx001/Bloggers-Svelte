b7f22d1a6f96ad8804cf4a2b7f363bc63af896be
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-07-04 15:18:13 +0100

    feat: implement data table with selection, filtering, and actions; add checkbox and action components

--- src/routes/(dashboard)/dashboard/categories/+page.svelte
+++ src/routes/(dashboard)/dashboard/categories/+page.svelte
@@ -1,9 +1,71 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
-
+	import DataTable from '$lib/components/data-tables/data-table.svelte';
+	import type { ColumnDef } from '@tanstack/table-core';
+	import { createRawSnippet } from 'svelte';
 	let { data }: PageProps = $props();
+	import { renderSnippet } from '$lib/components/ui/data-table/index.js';
+	import DataTableActions from '$lib/components/data-tables/data-table-actions.svelte';
+	import DataTableCheckbox from '$lib/components/data-tables/data-table-checkbox.svelte';
+	import { renderComponent } from '$lib/components/ui/data-table/index.js';
+	const columns: ColumnDef<Category>[] = [
+		{
+			id: 'select',
+			header: ({ table }) =>
+				renderComponent(DataTableCheckbox, {
+					checked: table.getIsAllPageRowsSelected(),
+					indeterminate: table.getIsSomePageRowsSelected() && !table.getIsAllPageRowsSelected(),
+					onCheckedChange: (value) => table.toggleAllPageRowsSelected(!!value),
+					'aria-label': 'Select all'
+				}),
+			cell: ({ row }) =>
+				renderComponent(DataTableCheckbox, {
+					checked: row.getIsSelected(),
+					onCheckedChange: (value) => row.toggleSelected(!!value),
+					'aria-label': 'Select row'
+				}),
+			enableSorting: false,
+			enableHiding: false
+		},
+		{
+			accessorKey: 'title',
+			header: 'Title'
+		},
+		{
+			accessorKey: 'description',
+			header: 'Description'
+		},
+		{
+			accessorKey: 'createdAt',
+			header: () => {
+				const createdAtHeaderSnippet = createRawSnippet(() => ({
+					render: () => `<div class="">Created At</div>`
+				}));
+				return renderSnippet(createdAtHeaderSnippet, '');
+			}
+		},
+		{
+			accessorKey: 'updatedAt',
+			header: () => {
+				const updatedAtHeaderSnippet = createRawSnippet(() => ({
+					render: () => `<div class="">Updated At</div>`
+				}));
+				return renderSnippet(updatedAtHeaderSnippet, '');
+			}
+		},
+		{
+			id: 'actions',
+			cell: ({ row }) => {
+				// You can pass whatever you need from `row.original` to the component
+				return renderComponent(DataTableActions, { id: row.original.id });
+			}
+		}
+	];
 </script>
 
-<pre class="pre">
-    {JSON.stringify(data.categories, null, 2)}
-</pre>
+<section class="p-6">
+	<DataTable data={data.categories?.content || []} {columns} />
+	<!-- <pre class="pre">
+		{JSON.stringify(data.categories, null, 2)}
+		</pre> -->
+</section>
