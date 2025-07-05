dcbb56752d7d6086117a1a39007195e9a7919595
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-07-05 16:40:20 +0100

    feat: add create category functionality with form validation and dialog component

--- src/routes/(dashboard)/dashboard/categories/+page.svelte
+++ src/routes/(dashboard)/dashboard/categories/+page.svelte
@@ -2,12 +2,21 @@
 	import type { PageProps } from './$types';
 	import DataTable from './data-table-categories.svelte';
 	import type { ColumnDef } from '@tanstack/table-core';
-	import { createRawSnippet } from 'svelte';
-	let { data }: PageProps = $props();
+	import { createRawSnippet, onMount } from 'svelte';
+	let { data, form }: PageProps = $props();
 	import { renderSnippet } from '$lib/components/ui/data-table/index.js';
 	import DataTableCheckbox from '$lib/components/data-tables/data-table-checkbox.svelte';
 	import { renderComponent } from '$lib/components/ui/data-table/index.js';
 	import DataTableCategoriesActions from './data-table-categories-actions.svelte';
+	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
+	import { Input } from '$lib/components/ui/input';
+	import { Plus } from '@lucide/svelte';
+
+	import * as Dialog from '$lib/components/ui/dialog';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { Textarea } from '$lib/components/ui/textarea/index.js';
+	import { enhance } from '$app/forms';
+
 	const columns: ColumnDef<Category>[] = [
 		{
 			id: 'select',
@@ -60,10 +69,54 @@
 			}
 		}
 	];
+
+	let createDialogOpen = $state(false);
 </script>
 
+{#snippet addCategory()}
+	<Dialog.Root bind:open={createDialogOpen}>
+		<Dialog.Trigger class={buttonVariants({ variant: 'outline' })}>
+			<Plus />
+			<span>Add Category</span>
+		</Dialog.Trigger>
+		<Dialog.Content class="sm:max-w-[425px]">
+			<Dialog.Header>
+				<Dialog.Title>Add Category</Dialog.Title>
+				<Dialog.Description>Fill the nessesary fields :</Dialog.Description>
+			</Dialog.Header>
+			<form action="?/createCategory" method="post" use:enhance>
+				<div class="grid gap-4 py-4">
+					<div class="grid grid-cols-4 items-center gap-4">
+						<Label for="title" class="text-right">Title*</Label>
+						<Input id="title" name="title" value="" placeholder="title" class="col-span-3" />
+					</div>
+					<div class="grid grid-cols-4 items-center gap-4">
+						<Label for="description" class="text-right">Description*</Label>
+						<Textarea name="description" placeholder="description" class="col-span-3" />
+					</div>
+				</div>
+
+				<Dialog.Footer>
+					<Button
+						onclick={() => {
+							if (form?.success) {
+								createDialogOpen = false;
+							}
+						}}
+						type="submit">Save changes</Button
+					>
+				</Dialog.Footer>
+			</form>
+		</Dialog.Content>
+	</Dialog.Root>
+{/snippet}
+
 <section class="p-6">
-	<DataTable showHeader data={data.categories?.content || []} {columns} />
+	<DataTable showHeader data={data.categories?.content || []} {columns}>
+		{#snippet trigger()}
+			{@render addCategory()}
+		{/snippet}
+	</DataTable>
 	<!-- <pre class="pre">
 		{JSON.stringify(data.categories, null, 2)}
 		</pre> -->
