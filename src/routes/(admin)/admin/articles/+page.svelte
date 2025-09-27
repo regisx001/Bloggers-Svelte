478db8a6d3bac46217fbd43e52576112ed2ebf66
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-09-27 17:18:16 +0100

    feat(articles): add line chart for desktop visitors over the last 6 months

--- src/routes/(admin)/admin/articles/+page.svelte
+++ src/routes/(admin)/admin/articles/+page.svelte
@@ -446,6 +446,19 @@
 		visitors: { label: 'Visitors' },
 		safari: { label: 'Safari', color: 'var(--chart-2)' }
 	} satisfies Chart.ChartConfig;
+
+	const chartData04 = [
+		{ date: new Date('2024-01-01'), desktop: 186 },
+		{ date: new Date('2024-02-01'), desktop: 305 },
+		{ date: new Date('2024-03-01'), desktop: 237 },
+		{ date: new Date('2024-04-01'), desktop: 73 },
+		{ date: new Date('2024-05-01'), desktop: 209 },
+		{ date: new Date('2024-06-01'), desktop: 214 }
+	];
+
+	const chartConfig04 = {
+		desktop: { label: 'Desktop', color: 'var(--chart-1)' }
+	} satisfies Chart.ChartConfig;
 </script>
 
 <!-- <pre>
@@ -704,6 +717,68 @@
 	</Card.Root>
 
 	<Card.Root class="grid grid-cols-3  border-0 bg-transparent">
+		<Card.Root class="col-span-2 flex">
+			<Card.Header>
+				<Card.Title>Line Chart - Label</Card.Title>
+				<Card.Description>Showing total visitors for the last 6 months</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<Chart.Container config={chartConfig04} class="aspect-auto h-[220px] w-full">
+					<LineChart
+						points={{ r: 4 }}
+						labels={{ offset: 12 }}
+						data={chartData04}
+						x="date"
+						axis="x"
+						xScale={scaleUtc()}
+						series={[
+							{
+								key: 'desktop',
+								label: 'Desktop',
+								color: chartConfig04.desktop.color
+							}
+						]}
+						props={{
+							spline: { curve: curveNatural, motion: 'tween', strokeWidth: 2 },
+							highlight: {
+								points: {
+									motion: 'none',
+									r: 6
+								}
+							},
+							xAxis: {
+								format: (v: Date) => v.toLocaleDateString('en-US', { month: 'short' }),
+								ticks: chartData04.length
+							}
+						}}
+					>
+						{#snippet tooltip()}
+							<Chart.Tooltip
+								labelFormatter={(v: Date) => {
+									return v.toLocaleDateString('en-US', {
+										month: 'long'
+									});
+								}}
+								indicator="line"
+							/>
+						{/snippet}
+					</LineChart>
+				</Chart.Container>
+			</Card.Content>
+			<Card.Footer>
+				<div class="flex w-full items-start gap-2 text-sm">
+					<div class="grid gap-2">
+						<div class="flex items-center gap-2 leading-none font-medium">
+							Trending up by 5.2% this month <TrendingUpIcon class="size-4" />
+						</div>
+						<div class="text-muted-foreground flex items-center gap-2 leading-none">
+							January - June 2024
+						</div>
+					</div>
+				</div>
+			</Card.Footer>
+		</Card.Root>
+
 		<Card.Root class="col-span-1 flex">
 			<Card.Header class="items-center">
 				<Card.Title>Radial Chart - Shape</Card.Title>
